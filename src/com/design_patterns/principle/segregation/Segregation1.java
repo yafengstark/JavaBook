ed2aeed29253319bfaaa9a8db545c139dd91e8e8
tony <fengfeng043@gmail.com>
2019-07-15 17:26:27 +0800

    接口依赖原则

--- src/com/design_patterns/principle/segregation/Segregation1.java
+++ src/com/design_patterns/principle/segregation/Segregation1.java
@@ -1,15 +1,21 @@
 package com.design_patterns.principle.segregation;
 
+/**
+ * 不符合接口隔离原则
+ *
+ *
+ *
+ */
 public class Segregation1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
 }
 
-//�ӿ�
+
 interface Interface1 {
 	void operation1();
 	void operation2();
@@ -20,43 +26,43 @@ interface Interface1 {
 
 class B implements Interface1 {
 	public void operation1() {
-		System.out.println("B ʵ���� operation1");
+		System.out.println("B 实现了 operation1");
 	}
 	
 	public void operation2() {
-		System.out.println("B ʵ���� operation2");
+		System.out.println("B 实现了 operation2");
 	}
 	public void operation3() {
-		System.out.println("B ʵ���� operation3");
+		System.out.println("B 实现了 operation3");
 	}
 	public void operation4() {
-		System.out.println("B ʵ���� operation4");
+		System.out.println("B 实现了 operation4");
 	}
 	public void operation5() {
-		System.out.println("B ʵ���� operation5");
+		System.out.println("B 实现了 operation5");
 	}
 }
 
 class D implements Interface1 {
 	public void operation1() {
-		System.out.println("D ʵ���� operation1");
+		System.out.println("D 实现了 operation1");
 	}
 	
 	public void operation2() {
-		System.out.println("D ʵ���� operation2");
+		System.out.println("D 实现了 operation2");
 	}
 	public void operation3() {
-		System.out.println("D ʵ���� operation3");
+		System.out.println("D 实现了 operation3");
 	}
 	public void operation4() {
-		System.out.println("D ʵ���� operation4");
+		System.out.println("D 实现了 operation4");
 	}
 	public void operation5() {
-		System.out.println("D ʵ���� operation5");
+		System.out.println("D 实现了 operation5");
 	}
 }
 
-class A { //A ��ͨ���ӿ�Interface1 ����(ʹ��) B�࣬����ֻ���õ�1,2,3����
+class A {
 	public void depend1(Interface1 i) {
 		i.operation1();
 	}
@@ -68,7 +74,7 @@ class A { //A ��ͨ���ӿ�Interface1 ����(ʹ��) B�࣬�
 	}
 }
   
-class C { //C ��ͨ���ӿ�Interface1 ����(ʹ��) D�࣬����ֻ���õ�1,4,5����
+class C {
 	public void depend1(Interface1 i) {
 		i.operation1();
 	}
--- src/com/design_patterns/principle/segregation/improve/Segregation1.java
+++ src/com/design_patterns/principle/segregation/improve/Segregation1.java
@@ -1,18 +1,23 @@
 package com.design_patterns.principle.segregation.improve;
 
+/**
+ * 符合接口隔离原则
+ *
+ * 接口最小
+ */
 public class Segregation1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// ʹ��һ��
+
+
 		A a = new A();
-		a.depend1(new B()); // A��ͨ���ӿ�ȥ����B��
+		a.depend1(new B());
 		a.depend2(new B());
 		a.depend3(new B());
 
 		C c = new C();
 
-		c.depend1(new D()); // C��ͨ���ӿ�ȥ����(ʹ��)D��
+		c.depend1(new D());
 		c.depend4(new D());
 		c.depend5(new D());
 
@@ -20,20 +25,20 @@ public class Segregation1 {
 
 }
 
-// �ӿ�1
+//
 interface Interface1 {
 	void operation1();
 
 }
 
-// �ӿ�2
+//
 interface Interface2 {
 	void operation2();
 
 	void operation3();
 }
 
-// �ӿ�3
+//
 interface Interface3 {
 	void operation4();
 
@@ -42,34 +47,34 @@ interface Interface3 {
 
 class B implements Interface1, Interface2 {
 	public void operation1() {
-		System.out.println("B ʵ���� operation1");
+		System.out.println("B 实现了 operation1");
 	}
 
 	public void operation2() {
-		System.out.println("B ʵ���� operation2");
+		System.out.println("B 实现了 operation2");
 	}
 
 	public void operation3() {
-		System.out.println("B ʵ���� operation3");
+		System.out.println("B 实现了 operation3");
 	}
 
 }
 
 class D implements Interface1, Interface3 {
 	public void operation1() {
-		System.out.println("D ʵ���� operation1");
+		System.out.println("D  operation1");
 	}
 
 	public void operation4() {
-		System.out.println("D ʵ���� operation4");
+		System.out.println("D  operation4");
 	}
 
 	public void operation5() {
-		System.out.println("D ʵ���� operation5");
+		System.out.println("D  operation5");
 	}
 }
 
-class A { // A ��ͨ���ӿ�Interface1,Interface2 ����(ʹ��) B�࣬����ֻ���õ�1,2,3����
+class A {
 	public void depend1(Interface1 i) {
 		i.operation1();
 	}
@@ -83,7 +88,7 @@ class A { // A ��ͨ���ӿ�Interface1,Interface2 ����(ʹ��
 	}
 }
 
-class C { // C ��ͨ���ӿ�Interface1,Interface3 ����(ʹ��) D�࣬����ֻ���õ�1,4,5����
+class C {
 	public void depend1(Interface1 i) {
 		i.operation1();
 	}
