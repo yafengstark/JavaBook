2af803f87f1fe3c293c2b79533d4631a15c49538
tony <fengfeng043@gmail.com>
2019-07-15 22:44:59 +0800

    迪米特法则

--- src/com/design_patterns/principle/demeter/Demeter1.java
+++ src/com/design_patterns/principle/demeter/Demeter1.java
@@ -3,98 +3,98 @@ package com.design_patterns.principle.demeter;
 import java.util.ArrayList;
 import java.util.List;
 
-//�ͻ���
+//客户端
 public class Demeter1 {
 
-	public static void main(String[] args) {
-		//������һ�� SchoolManager ����
-		SchoolManager schoolManager = new SchoolManager();
-		//���ѧԺ��Ա��id ��  ѧУ�ܲ���Ա����Ϣ
-		schoolManager.printAllEmployee(new CollegeManager());
+    public static void main(String[] args) {
+        //创建了一个 SchoolManager 对象
+        SchoolManager schoolManager = new SchoolManager();
+        //输出学院的员工id 和  学校总部的员工信息
+        schoolManager.printAllEmployee(new CollegeManager());
 
-	}
+    }
 
 }
 
 
-//ѧУ�ܲ�Ա����
+//学校总部员工类
 class Employee {
-	private String id;
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 }
 
 
-//ѧԺ��Ա����
+//学院的员工类
 class CollegeEmployee {
-	private String id;
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 }
 
 
-//����ѧԺԱ���Ĺ�����
+//管理学院员工的管理类
 class CollegeManager {
-	//����ѧԺ������Ա��
-	public List<CollegeEmployee> getAllEmployee() {
-		List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
-		for (int i = 0; i < 10; i++) { //��������������10��Ա���� list
-			CollegeEmployee emp = new CollegeEmployee();
-			emp.setId("ѧԺԱ��id= " + i);
-			list.add(emp);
-		}
-		return list;
-	}
+    //返回学院的所有员工
+    public List<CollegeEmployee> getAllEmployee() {
+        List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
+        for (int i = 0; i < 10; i++) { //这里我们增加了10个员工到 list
+            CollegeEmployee emp = new CollegeEmployee();
+            emp.setId("学院员工id= " + i);
+            list.add(emp);
+        }
+        return list;
+    }
 }
 
-//ѧУ������
+//学校管理类
 
-//���� SchoolManager ���ֱ������������Щ Employee��CollegeManager
-//CollegeEmployee ���� ֱ������ ����һ��İ���࣬����Υ���� �����ط��� 
+//分析 SchoolManager 类的直接朋友类有哪些 Employee、CollegeManager
+//CollegeEmployee 不是 直接朋友 而是一个陌生类，这样违背了 迪米特法则
 class SchoolManager {
-	//����ѧУ�ܲ���Ա��
-	public List<Employee> getAllEmployee() {
-		List<Employee> list = new ArrayList<Employee>();
-		
-		for (int i = 0; i < 5; i++) { //��������������5��Ա���� list
-			Employee emp = new Employee();
-			emp.setId("ѧУ�ܲ�Ա��id= " + i);
-			list.add(emp);
-		}
-		return list;
-	}
+    //返回学校总部的员工
+    public List<Employee> getAllEmployee() {
+        List<Employee> list = new ArrayList<Employee>();
 
-	//�÷���������ѧУ�ܲ���ѧԺԱ����Ϣ(id)
-	void printAllEmployee(CollegeManager sub) {
-		
-		//��������
-		//1. ����� CollegeEmployee ����  SchoolManager��ֱ������
-		//2. CollegeEmployee ���Ծֲ�������ʽ������ SchoolManager
-		//3. Υ���� �����ط��� 
-		
-		//��ȡ��ѧԺԱ��
-		List<CollegeEmployee> list1 = sub.getAllEmployee();
-		System.out.println("------------ѧԺԱ��------------");
-		for (CollegeEmployee e : list1) {
-			System.out.println(e.getId());
-		}
-		//��ȡ��ѧУ�ܲ�Ա��
-		List<Employee> list2 = this.getAllEmployee();
-		System.out.println("------------ѧУ�ܲ�Ա��------------");
-		for (Employee e : list2) {
-			System.out.println(e.getId());
-		}
-	}
+        for (int i = 0; i < 5; i++) { //这里我们增加了5个员工到 list
+            Employee emp = new Employee();
+            emp.setId("学校总部员工id= " + i);
+            list.add(emp);
+        }
+        return list;
+    }
+
+    //该方法完成输出学校总部和学院员工信息(id)
+    void printAllEmployee(CollegeManager sub) {
+
+        //分析问题
+        //1. 这里的 CollegeEmployee 不是  SchoolManager的直接朋友
+        //2. CollegeEmployee 是以局部变量方式出现在 SchoolManager
+        //3. 违反了 迪米特法则
+
+        //获取到学院员工
+        List<CollegeEmployee> list1 = sub.getAllEmployee();
+        System.out.println("------------学院员工------------");
+        for (CollegeEmployee e : list1) {
+            System.out.println(e.getId());
+        }
+        //获取到学校总部员工
+        List<Employee> list2 = this.getAllEmployee();
+        System.out.println("------------学校总部员工------------");
+        for (Employee e : list2) {
+            System.out.println(e.getId());
+        }
+    }
 }
--- src/com/design_patterns/principle/demeter/improve/Demeter1.java
+++ src/com/design_patterns/principle/demeter/improve/Demeter1.java
@@ -3,14 +3,14 @@ package com.design_patterns.principle.demeter.improve;
 import java.util.ArrayList;
 import java.util.List;
 
-//�ͻ���
+//客户端
 public class Demeter1 {
 
 	public static void main(String[] args) {
-		System.out.println("~~~ʹ�õ����ط���ĸĽ�~~~");
-		//������һ�� SchoolManager ����
+		System.out.println("~~~使用迪米特法则的改进~~~");
+		//创建了一个 SchoolManager 对象
 		SchoolManager schoolManager = new SchoolManager();
-		//���ѧԺ��Ա��id ��  ѧУ�ܲ���Ա����Ϣ
+		//输出学院的员工id 和  学校总部的员工信息
 		schoolManager.printAllEmployee(new CollegeManager());
 
 	}
@@ -18,7 +18,7 @@ public class Demeter1 {
 }
 
 
-//ѧУ�ܲ�Ա����
+//学校总部员工类
 class Employee {
 	private String id;
 
@@ -32,7 +32,7 @@ class Employee {
 }
 
 
-//ѧԺ��Ա����
+//学院的员工类
 class CollegeEmployee {
 	private String id;
 
@@ -46,57 +46,57 @@ class CollegeEmployee {
 }
 
 
-//����ѧԺԱ���Ĺ�����
+//管理学院员工的管理类
 class CollegeManager {
-	//����ѧԺ������Ա��
+	//返回学院的所有员工
 	public List<CollegeEmployee> getAllEmployee() {
 		List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
-		for (int i = 0; i < 10; i++) { //��������������10��Ա���� list
+		for (int i = 0; i < 10; i++) { //这里我们增加了10个员工到 list
 			CollegeEmployee emp = new CollegeEmployee();
-			emp.setId("ѧԺԱ��id= " + i);
+			emp.setId("学院员工id= " + i);
 			list.add(emp);
 		}
 		return list;
 	}
-	
-	//���ѧԺԱ������Ϣ
+
+	//输出学院员工的信息
 	public void printEmployee() {
-		//��ȡ��ѧԺԱ��
+		//获取到学院员工
 		List<CollegeEmployee> list1 = getAllEmployee();
-		System.out.println("------------ѧԺԱ��------------");
+		System.out.println("------------学院员工------------");
 		for (CollegeEmployee e : list1) {
 			System.out.println(e.getId());
 		}
 	}
 }
 
-//ѧУ������
+//学校管理类
 
-//���� SchoolManager ���ֱ������������Щ Employee��CollegeManager
-//CollegeEmployee ���� ֱ������ ����һ��İ���࣬����Υ���� �����ط��� 
+//分析 SchoolManager 类的直接朋友类有哪些 Employee、CollegeManager
+//CollegeEmployee 不是 直接朋友 而是一个陌生类，这样违背了 迪米特法则
 class SchoolManager {
-	//����ѧУ�ܲ���Ա��
+	//返回学校总部的员工
 	public List<Employee> getAllEmployee() {
 		List<Employee> list = new ArrayList<Employee>();
-		
-		for (int i = 0; i < 5; i++) { //��������������5��Ա���� list
+
+		for (int i = 0; i < 5; i++) { //这里我们增加了5个员工到 list
 			Employee emp = new Employee();
-			emp.setId("ѧУ�ܲ�Ա��id= " + i);
+			emp.setId("学校总部员工id= " + i);
 			list.add(emp);
 		}
 		return list;
 	}
 
-	//�÷���������ѧУ�ܲ���ѧԺԱ����Ϣ(id)
+	//该方法完成输出学校总部和学院员工信息(id)
 	void printAllEmployee(CollegeManager sub) {
-		
-		//��������
-		//1. �����ѧԺ��Ա����������װ��CollegeManager
+
+		//分析问题
+		//1. 将输出学院的员工方法，封装到CollegeManager
 		sub.printEmployee();
-	
-		//��ȡ��ѧУ�ܲ�Ա��
+
+		//获取到学校总部员工
 		List<Employee> list2 = this.getAllEmployee();
-		System.out.println("------------ѧУ�ܲ�Ա��------------");
+		System.out.println("------------学校总部员工------------");
 		for (Employee e : list2) {
 			System.out.println(e.getId());
 		}
