35567dd1a051a07676e885ab547a538e84bde13c
tony <fengfeng043@gmail.com>
2019-07-16 22:52:19 +0800

    桥接模式

---
+++ src/com/design_patterns/bridge/Brand.java
@@ -0,0 +1,9 @@
+package com.design_patterns.bridge;
+
+//接口
+public interface Brand {
+	void open();
+	void close();
+	void call();
+}
+
---
+++ src/com/design_patterns/bridge/Vivo.java
@@ -0,0 +1,23 @@
+package com.design_patterns.bridge;
+
+public class Vivo implements Brand {
+
+	@Override
+	public void open() {
+
+		System.out.println(" Vivo手机开机 ");
+	}
+
+	@Override
+	public void close() {
+
+		System.out.println(" Vivo手机关机 ");
+	}
+
+	@Override
+	public void call() {
+
+		System.out.println(" Vivo手机打电话 ");
+	}
+
+}
