c509f4f59107f81e4c2b61d9ca4d8f3b184208f7
tony <fengfeng043@gmail.com>
2019-07-16 17:21:56 +0800

    简单工厂

---
+++ src/com/design_patterns/factory/simplefactory/pizzastore/pizza/PepperPizza.java
@@ -0,0 +1,13 @@
+package com.design_patterns.factory.simplefactory.pizzastore.pizza;
+
+
+public class PepperPizza extends Pizza {
+
+	@Override
+	public void prepare() {
+
+		System.out.println(" 给胡椒披萨准备原材料 ");
+	}
+
+}
+
