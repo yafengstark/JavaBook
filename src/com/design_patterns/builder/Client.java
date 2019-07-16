a25a1b7fc4cc5bee869a33ff34ae12c8f1d0da9b
tony <fengfeng043@gmail.com>
2019-07-16 21:40:16 +0800

    建造者模式

---
+++ src/com/design_patterns/builder/Client.java
@@ -0,0 +1,11 @@
+package com.design_patterns.builder;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		CommonHouse commonHouse = new CommonHouse();
+		commonHouse.build();
+	}
+
+}
---
+++ src/com/design_patterns/builder/StringBuilderDemo.java
@@ -0,0 +1,12 @@
+package com.design_patterns.builder;
+
+/**
+ * @author tony
+ * @date 2019/7/16 21:22
+ */
+public class StringBuilderDemo {
+
+    public static void main(String[] args){
+        StringBuilder stringBuffer = new StringBuilder("hello");
+    }
+}
