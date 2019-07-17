c34e5c8144d7f04ca1aa66d042280e04f31b67a5
tony <fengfeng043@gmail.com>
2019-07-17 23:42:08 +0800

    迭代器模式

---
+++ src/com/design_patterns/iterator/College.java
@@ -0,0 +1,14 @@
+package com.design_patterns.iterator;
+
+import java.util.Iterator;
+
+public interface College {
+
+	 String getName();
+
+	//增加系的方法
+	 void addDepartment(String name, String desc);
+
+	//返回一个迭代器,遍历
+	 Iterator  createIterator();
+}
