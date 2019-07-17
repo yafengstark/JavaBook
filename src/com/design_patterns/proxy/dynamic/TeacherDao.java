c7ddfc0eebdbd0d73544fa9f86289823373ab2c7
tony <fengfeng043@gmail.com>
2019-07-17 21:58:05 +0800

    动态代理

---
+++ src/com/design_patterns/proxy/dynamic/TeacherDao.java
@@ -0,0 +1,18 @@
+package com.design_patterns.proxy.dynamic;
+
+public class TeacherDao implements ITeacherDao {
+
+	@Override
+	public void teach() {
+
+		System.out.println(" 老师授课中.... ");
+	}
+
+	@Override
+	public void sayHello(String name) {
+
+		System.out.println("hello " + name);
+	}
+
+}
+
