445b85225729c553a7f7723d4bcb36480e06d75a
tony <fengfeng043@gmail.com>
2019-07-17 20:18:43 +0800

    外观模式

---
+++ src/com/design_patterns/facade/Client.java
@@ -0,0 +1,16 @@
+package com.design_patterns.facade;
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//这里直接调用。。 很麻烦
+		HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade();
+		homeTheaterFacade.ready();
+		homeTheaterFacade.play();
+
+
+		homeTheaterFacade.end();
+	}
+
+}
+
---
+++ src/com/design_patterns/facade/Screen.java
@@ -0,0 +1,21 @@
+package com.design_patterns.facade;
+
+public class Screen {
+
+	private static Screen instance = new Screen();
+	
+	public static Screen getInstance() {
+		return instance;
+	}
+
+	
+	public void up() {
+		System.out.println(" Screen up ");
+	}
+	
+	public void down() {
+		System.out.println(" Screen down ");
+	}
+	
+
+}
